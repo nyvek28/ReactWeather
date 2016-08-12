b2e8d1c4c8e1c6fe3b14c96fb0c7fd9f57bfcb63
Kevyn <kevyn@firstfactory.com>
2016-08-12 13:23:17 -0600

    Ready app for heroku

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express'),
     app = express(),
-    port = 3000;
+    port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  }else{
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 app.use(express.static('public'));
 
 app.listen(port, function(){
