a1a9be7be581f3fc0f2a422ed68ecea772090fa4
Kevyn <kevyn@firstfactory.com>
2016-08-12 13:53:32 -0600

    Updated title, Examples, and About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,14 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h3>About</h3>
+      <p>
+        Hi, this is a sample app built by developer <a href = "http://www.kevynquiros.com">Kevyn Quiros</a>.
+        This app was built on top of React framework as a learning task
+        and it's source is fully available <a href = "https://github.com/nyvek28/ReactWeather">here</a>.
+      </p>
+    </div>
   );
 }
 
