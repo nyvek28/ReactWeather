cb0499458be895e9303dc0521a7788f5f46f9a85
Kevyn <kevyn@firstfactory.com>
2016-08-16 08:14:10 -0600

    Config updates, about to enter styling

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,10 @@ var React = require('react'),
     Examples = require ('Examples'),
     {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+//Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
 ReactDOM.render(
   <Router history = {hashHistory}>
     <Route path = "/" component = {Main}>
